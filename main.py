e2de98a6d4486f60bdb595f32dfdbd91abae4c16
Trimonix <the.lil.flopper@gmail.com>
2018-06-16 02:04:53 +1000

    Modularisation of interface and main file

--- main.py
+++ main.py
@@ -12,129 +12,20 @@ from time import *
 import io
 import sys
 import importlib
+import interface
 
 pygame.init()
 
 screen = pygame.display.set_mode((480, 270))#, pygame.FULLSCREEN)
 
-# Font type and size
-font = pygame.font.Font('OCRAEXT.ttf', 18)
-# Declares clock
-clock = pygame.time.Clock()
-
-#Color palette
-BLUE_IO = ( 23,  53, 109)
-OUTLINE = (252,  79,   0)
-BLUE_F =  ( 23,  96, 109)
-TEXT_M =  (255, 255, 255)
-
-class Box:
-    def __init__(self, text, rect):#x, y, w, h):
-        self.text = text
-        self.box = pygame.Surface(rect[2:])
-        self.pos = rect[:2]
-        self.default_pos = rect[:2]
-        self.module = None
-
-    def draw(self, selected):
-        if selected:
-            self.box.fill(BLUE_F)
-
-            text = font.render(self.text, True, TEXT_M)
-            text_pos = [(self.box.get_size()[a] - text.get_size()[a])//2 for a in [0, 1]]
-            self.box.blit(text, text_pos)
-
-            # Border
-            pygame.draw.rect(self.box, OUTLINE, self.box.get_rect(), 3)
-        else:
-            self.box.fill(BLUE_F)
-
-            text = font.render(self.text, True, TEXT_M)
-            text_pos = [(self.box.get_size()[a] - text.get_size()[a])//2 for a in [0, 1]]
-            self.box.blit(text, text_pos)
-
-            # Border
-            pygame.draw.rect(self.box, BLUE_IO, self.box.get_rect(), 3)
-
-
-
-    def update(self, scroll):
-        self.pos[0] = self.default_pos[0] + scroll
-        pass
-
-# Selection is [x, y]
-selection = [0, 0]
-
-scroll_amount = 0
-
-BG = pygame.image.load('BG_Main.png').convert()
-
-programs = []
-for line in open('modules/programs.txt'):
-    line = line.strip()
-    try:
-        module = importlib.import_module("modules."+line)
-        programs.append((module.NAME, module))
-    except ImportError:
-        print(line, 'failed to import')
-
-boxes = []
-programs.sort()
-for p in range(0, len(programs), 2):
-    column = []
-    box = Box(programs[p][0], [70+p//2*190, 55, 150, 70])
-    box.module = programs[p][1]
-    column.append(box)
-    if p+1 < len(programs):
-        box = Box(programs[p+1][0], [70+p//2*190, 145, 150, 70])
-        box.module = programs[p+1][1]
-        column.append(box)
-    boxes.append(column)
-
-
-while not False and True and 1 and not 0 and 1.0 and not 0.0 and not '' and 'hello' and ['meh'] and not [] and ...:
+while eval('not '*100 + 'True'):
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            if is_rpi:
-                button_driver.shutdown()
             pygame.quit()
             sys.exit()
-
         elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_UP:
-                selection[1] -= 1
-                selection[1] %= 2
-            if event.key == pygame.K_DOWN:
-                selection[1] += 1
-                selection[1] %= 2
-            if event.key == pygame.K_LEFT:
-                selection[0] -= 1
-                selection[0] %= len(boxes)
-            if event.key == pygame.K_RIGHT:
-                selection[0] += 1
-                selection[0] %= len(boxes)
-            if event.key == pygame.K_RETURN:
+            if event.key == pygame.K_h:
                 pass
 
-            if len(boxes[selection[0]]) == 1 and selection[1]:
-                selection[1] = 0
-
-
-    screen.fill((0, 0, 0))
-    # if box.pos[0] > 190*(len(column)):
-    #     box.pos[0] = 190*(len(column))
-
-    if scroll_amount > 0:
-        scroll_amount = 0
-    screen.blit(BG, [0, 0])
-    # Draw boxes
-    scroll_amount = -190 * selection[0]
-    for x, column in enumerate(boxes):
-        for y, box in enumerate(column):
-            box.update(scroll_amount)
-            box.draw([x, y] == selection)
-            screen.blit(box.box, box.pos)
-
-    pygame.display.flip()
-
-    clock.tick(30)
+    interface = interface.run(screen)
+    print('interface returned')
--- modules/music.py
+++ modules/music.py
@@ -5,3 +5,50 @@ import io
 import sys
 
 NAME = 'Music'
+
+def run(screen):
+
+    # Font type and size
+    font = pygame.font.Font('OCRAEXT.ttf', 18)
+    # Declares clock
+    clock = pygame.time.Clock()
+    
+    class Mboxes:
+        def __init__(self, text, rect):
+            self.text = text
+            self.box = pygame.Surface(rect[2:])
+            self.pos = rect[:2]
+            self.default_pos = rect[:2]
+            self.file = None
+
+        def draw(self, selected):
+            if selected:
+                self.box.fill(BLUE_F)
+
+                text = font.render(self.text, True, TEXT_M)
+                text_pos = [(self.box.get_size()[a] - text.get_size()[a])//2 for a in [0, 1]]
+                self.box.blit(text, text_pos)
+
+                # Border
+                pygame.draw.rect(self.box, OUTLINE, self.box.get_rect(), 3)
+            else:
+                self.box.fill(BLUE_F)
+
+                text = font.render(self.text, True, TEXT_M)
+                text_pos = [(self.box.get_size()[a] - text.get_size()[a])//2 for a in [0, 1]]
+                self.box.blit(text, text_pos)
+
+                # Border
+                pygame.draw.rect(self.box, BLUE_IO, self.box.get_rect(), 3)
+
+
+
+        def update(self, scroll):
+            self.pos[0] = self.default_pos[0] + scroll
+
+
+
+
+    while True:
+        screen.fill((0, 0, 0))
+        pygame.display.flip()
