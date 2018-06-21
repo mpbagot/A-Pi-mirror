93f89a28ebc839849208f1ec90f0cc77d9a311e6
Mitchell Bagot <packof14eggies@gmail.com>
2018-06-21 22:54:58 +1000

    Update template

--- modules/template.py
+++ modules/template.py
@@ -4,54 +4,59 @@ import io
 import sys
 sys.path.append('../')
 
+pygame.init()
+# Font type and size
+font = pygame.font.Font('OCRAEXT.ttf', 9)
+# Declares clock
+clock = pygame.time.Clock()
+
+# Color palette
+BLUE_IO = ( 23,  53, 109)
+OUTLINE = (252,  79,   0)
+BLUE_F =  ( 23,  96, 109)
+TEXT_M =  (255, 255, 255)
+
+# Main background
+BG = pygame.image.load('BG_Main.png').convert()
 
 NAME = 'INSERT MODULE NAME HERE'
 
-def run(screen):
-    pygame.init()
-    # Font type and size
-    font = pygame.font.Font('OCRAEXT.ttf', 9)
-    # Declares clock
-    clock = pygame.time.Clock()
+def setup():
+    '''
+    Set up any variables for this instance of the interface.
+    This function is re-run each time the interface is reopened.
+    '''
+    pass
 
-    # Color palette
-    BLUE_IO = ( 23,  53, 109)
-    OUTLINE = (252,  79,   0)
-    BLUE_F =  ( 23,  96, 109)
-    TEXT_M =  (255, 255, 255)
+def step(screen, events):
+    '''
+    Perform on draw frame of updates.
+    Return False to exit the interface and return to main menu, or a module object to open the corresponding interface.
+    '''
+    for event in events:
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                pass
+            if event.key == pygame.K_DOWN:
+                pass
+            if event.key == pygame.K_LEFT:
+                pass
+            if event.key == pygame.K_RIGHT:
+                pass
+            # Enter button
+            if event.key == pygame.K_RETURN:
+                pass
+            # Back button
+            if event.key == pygame.K_BACKSPACE:
+                pass
 
-    # Main background
-    BG = pygame.image.load('BG_Main.png').convert()
+    screen.fill((0, 0, 0))
+    screen.blit(BG, [0, 0])
 
-    while True:
-        for event in pygame.event.get():
+    """
+    Screen contents go here
+    """
 
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_UP:
-                    pass
-                if event.key == pygame.K_DOWN:
-                    pass
-                if event.key == pygame.K_LEFT:
-                    pass
-                if event.key == pygame.K_RIGHT:
-                    pass
-                # Enter button
-                if event.key == pygame.K_RETURN:
-                    pass
-                # Back button
-                if event.key == pygame.K_BACKSPACE:
-                    pass
-                # Home button
-                if event.key == pygame.K_h:
-                    pass
+    pygame.display.flip()
 
-        screen.fill((0, 0, 0))
-        screen.blit(BG, [0, 0])
-
-        """
-        Screen contents go here
-        """
-
-        pygame.display.flip()
-
-        clock.tick(30)
+    clock.tick(30)
