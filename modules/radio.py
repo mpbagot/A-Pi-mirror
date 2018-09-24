53223e20da587e60aa596b8c0da183c504b87c6d
Mitchell Bagot <packof14eggies@gmail.com>
2018-09-24 20:55:59 +1000

    Fix radio module to match new module structure

--- modules/radio.py
+++ modules/radio.py
@@ -4,46 +4,47 @@ from time import *
 import io
 import sys
 
+# Font type and size
+font = pygame.font.Font('resources/font/ocraext.ttf', 9)
+# Declares clock
+clock = pygame.time.Clock()
+
+# Color palette
+BLUE_IO = ( 23,  53, 109)
+OUTLINE = (252,  79,   0)
+BLUE_F =  ( 23,  96, 109)
+TEXT_M =  (255, 255, 255)
+
+# Main background
+BG = pygame.image.load('resources/textures/bg_main.png').convert()
+
 NAME = 'Radio'
-def run(screen):
-    pygame.init()
-    # Font type and size
-    font = pygame.font.Font('resources/font/ocraext.ttf', 9)
-    # Declares clock
-    clock = pygame.time.Clock()
+def setup():
+    pass
 
-    # Color palette
-    BLUE_IO = ( 23,  53, 109)
-    OUTLINE = (252,  79,   0)
-    BLUE_F =  ( 23,  96, 109)
-    TEXT_M =  (255, 255, 255)
+def step(screen, events):
+    for event in events:
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                pass
+            if event.key == pygame.K_DOWN:
+                pass
+            if event.key == pygame.K_LEFT:
+                pass
+            if event.key == pygame.K_RIGHT:
+                pass
+            # Enter button
+            if event.key == pygame.K_RETURN:
+                pass
+            # Back button
+            if event.key == pygame.K_BACKSPACE:
+                pass
 
-    # Main background
-    BG = pygame.image.load('resources/textures/bg_main.png').convert()
+    screen.fill((0, 0, 0))
+    screen.blit(BG, [0, 0])
 
-    while True:
-        for event in pygame.event.get():
+    """
+    Screen contents go here
+    """
 
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_UP:
-                    pass
-                if event.key == pygame.K_DOWN:
-                    pass
-                if event.key == pygame.K_LEFT:
-                    pass
-                if event.key == pygame.K_RIGHT:
-                    pass
-                # Enter button
-                if event.key == pygame.K_RETURN:
-                    pass
-                # Back button
-                if event.key == pygame.K_BACKSPACE:
-                    pass
-        screen.fill((0, 0, 0))
-        screen.blit(BG, [0, 0])
-
-        """
-        Screen contents go here
-        """
-
-        clock.tick(30)
+    clock.tick(30)
